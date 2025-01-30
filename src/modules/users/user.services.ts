7ad30aa3490d50fc0a3d6f3314e9b6b275d612af
Carloshpjacinto <carlo.henrique37@gmail.com>
2025-01-29 22:37:17 -0300

    feature/criacao-DTO-user

--- src/modules/users/user.services.ts
+++ src/modules/users/user.services.ts
@@ -1,20 +1,33 @@
 import { User } from '@prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
+import { CreateUserDTO } from './domain/dto/createUser.dto';
+import { UpdateUserDTO } from './domain/dto/updateUser.dto';
 
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
 
-  createUser(body: any): Promise<User> {
-    return this.prisma.user.create({ data: body });
+  async createUser(body: CreateUserDTO): Promise<User> {
+    return await this.prisma.user.create({ data: body });
   }
 
-  list() {
-    return this.prisma.user.findMany();
+  async list() {
+    return await this.prisma.user.findMany();
   }
 
-  show(id: string) {
-    return this.prisma.user.findUnique({ where: { id: Number(id) } });
+  async show(id: string) {
+    return await this.prisma.user.findUnique({ where: { id: Number(id) } });
+  }
+
+  async update(id: string, body: UpdateUserDTO) {
+    return await this.prisma.user.update({
+      where: { id: Number(id) },
+      data: body,
+    });
+  }
+
+  async delete(id: string) {
+    return await this.prisma.user.delete({ where: { id: Number(id) } });
   }
 }
