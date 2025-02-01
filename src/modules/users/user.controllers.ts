a6371457044499212d1f58f651f82044bae76c0e
Carloshpjacinto <carlo.henrique37@gmail.com>
2025-01-31 22:34:15 -0300

    criando-config-decorators-e-guards

--- src/modules/users/user.controllers.ts
+++ src/modules/users/user.controllers.ts
@@ -13,40 +13,51 @@ import { UpdateUserDTO } from './domain/dto/updateUser.dto';
 import { ParamId } from 'src/shared/decorators/paramId.decorator';
 import { AuthGuard } from 'src/shared/guards/auth.guard';
 import { User } from 'src/shared/decorators/user.decorator';
-import { User as UserType } from '@prisma/client';
+import { Role, User as UserType } from '@prisma/client';
+import { Roles } from 'src/shared/decorators/roles.decorators';
+import { UserMatchGuard } from 'src/shared/guards/userMatch.guard';
+import { RoleGuard } from 'src/shared/guards/role.guard';
 
-@UseGuards(AuthGuard)
+@UseGuards(AuthGuard, RoleGuard)
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
 
   @Get()
   async list(@User() user: UserType) {
-    console.log(user)
+    console.log(user);
     return await this.userService.list();
   }
 
   @Get(':id')
   async show(@User() user: UserType, @ParamId() id: number) {
-    console.log(user)
+    console.log(user);
     return await this.userService.show(id);
   }
 
+  @Roles(Role.ADMIN)
   @Post()
   async createUser(@User() user: UserType, @Body() body: CreateUserDTO) {
-    console.log(user)
+    console.log(user);
     return await this.userService.createUser(body);
   }
 
+  @UseGuards(UserMatchGuard)
+  @Roles(Role.ADMIN, Role.USER)
   @Patch(':id')
-  updateUser(@User() user: UserType, @ParamId() id: number, @Body() body: UpdateUserDTO) {
-    console.log(user)
+  updateUser(
+    @User() user: UserType,
+    @ParamId() id: number,
+    @Body() body: UpdateUserDTO,
+  ) {
+    console.log(user);
     return this.userService.update(id, body);
   }
 
+  @UseGuards(UserMatchGuard)
   @Delete(':id')
   deleteUser(@User() user: UserType, @ParamId() id: number) {
-    console.log(user)
+    console.log(user);
     return this.userService.delete(id);
   }
 }
